93ea9ec66be5b40e8925c58de71b8e32e82e5fe8
cjhedges93 <159423755+cjhedges93@users.noreply.github.com>
2024-02-14 10:39:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-var el = document.getElementById('login');
-el.onclick = workingOnIt;
+var client_id = '1a56f0ddf7e04b659519f1fcc5f622f8';
+var redirect_uri = 'http://localhost:8888/callback';
 
-function workingOnIt() {
-      alert('Working on it!);
-      return false;
+var app = express();
+
+document.getElementById('login') function{
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+}
+
+
 }
